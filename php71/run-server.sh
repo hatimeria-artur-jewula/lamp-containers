aab5284db58c179d3c3884d13ac8763b80bfb055
Artur Jewuła <artur.jewula@hatimeria.pl>
2019-02-14 23:21:32 +0100

    make mysql work;add default admin account; add bcmath module

--- php71/run-server.sh
+++ php71/run-server.sh
@@ -4,7 +4,8 @@ service ssh start
 service rsyslog start
 service nginx start
 service php7.1-fpm start
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
+service mysql stop
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql restart
 cron
 
 tail -f /dev/null
