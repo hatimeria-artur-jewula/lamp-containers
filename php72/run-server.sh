9c6cc51424a12fc4384714148f4f4d3d300b7301
Artur Jewuła <artur.jewula@hatimeria.pl>
2020-01-21 11:55:53 +0100

    Add ioncbue

--- php72/run-server.sh
+++ php72/run-server.sh
@@ -3,7 +3,7 @@
 service ssh start
 service rsyslog start
 service nginx start
-service php7.1-fpm start
+service php7.2-fpm start
 service mysql stop
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql restart
 cron
