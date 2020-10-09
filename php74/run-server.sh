5243d6136914f0ff82e2fd9997fda1cc90f57a45
Artur Jewuła <artur.jewula@hatimeria.pl>
2020-10-09 09:53:31 +0200

    Update php 7.3, prepare images for php 7.4

---
+++ php74/run-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+service ssh start
+service rsyslog start
+service nginx start
+service php7.4-fpm start
+service mysql stop
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql restart
+cron
+
+tail -f /dev/null
